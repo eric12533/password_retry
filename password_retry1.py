41432b08b8559b851cc9eabfed0605a9d7634702
Eric Wang <eric533528@gmail.com>
2022-07-12 17:16:59 +0800

    Basic Version Done

--- password_retry1.py
+++ password_retry1.py
@@ -1,6 +1,6 @@
 password = 'a123456'
 c = 3
-while True:
+while c > 0:
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登陸成功')
@@ -8,5 +8,4 @@ while True:
 	else:
 		c -= 1
 		print('輸入錯誤!還剩',c,'次機會')
-		if c==0:
-			break
+
